164b06d651603740b5e378ff774e9ccce906aaaf
iirfanr <irfan.ramadhan021006@gmail.com>
2026-02-22 15:00:57 +0700

    more

--- jar file test/temp/src/test.java
+++ jar file test/temp/src/test.java
@@ -1,5 +1,7 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import id.ac.polban.employee.*;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("JAR berhasil dimasukkan!");
     }
 }
